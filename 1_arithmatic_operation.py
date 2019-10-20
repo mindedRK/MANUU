7e796d6b263301df2222c660d0d169e61494f599
RIYAZUDDIN KHAN <56753748+mindedRK@users.noreply.github.com>
2019-10-20 16:52:39 +0530

    Add files via upload

---
+++ 1_arithmatic_operation.py
@@ -0,0 +1,13 @@
+x = float(input("enter first number="))
+y = float(input("enter second number="))
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+remin=x%y
+print("addition of number is=",add)
+print("subtraction of number is=",sub)
+print("multiplecation of number is =",mul)        
+print("division of number is=",div)
+print("reminder of number is=",remin)        
+        
