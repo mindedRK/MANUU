6e43d8f46cfb2820646039fb735c9862bf1c6a10
RIYAZUDDIN KHAN <56753748+mindedRK@users.noreply.github.com>
2019-10-20 16:54:17 +0530

    Add files via upload

---
+++ 2_max_min.py
@@ -0,0 +1,5 @@
+l=[23,56,67,20]
+m=max(l)
+n=min(l)
+print(m)
+print(n)
---
+++ 3_sum_of_digit.py
@@ -0,0 +1,9 @@
+n=int(input("enter the number="))
+sum=0
+while(n>0):
+ r=n%10
+ sum=sum+r
+ n=(n//10)
+ 
+print('sum of digit=',sum)
+
